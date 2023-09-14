0f681809827c147e067b24500dde521c1727ff46
María Elena Haydeé Ponce <ponce.me@hotmail.com>
2023-09-14 17:56:50 -0300

    Java_Clase2_Ciclos

---
+++ Programacion_II/Clase_3/Leccion_Clases_Objetos/src/Clases/Persona.java
@@ -0,0 +1,12 @@
+package Clases;
+
+public class Persona {
+//Debe ser publica para poder acceder desde otros archivos
+//Palabra reservada class
+//Pascal Cases
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
