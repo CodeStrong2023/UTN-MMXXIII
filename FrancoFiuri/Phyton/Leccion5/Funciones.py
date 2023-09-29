7fab1cc45af0f28ff387cac7eab65bca73044440
Franco Fiuri <franco.fiuri@gmail.com>
2023-09-29 18:57:23 -0300

    Agregando archivos de Leccion 5 - Phyton

---
+++ FrancoFiuri/Phyton/Leccion5/Funciones.py
@@ -0,0 +1,6 @@
+# Funciones
+# Definimos una función
+def mi_funcion():   # Se debe respetar la tabulacion para que este dentro de la función.
+    print('Hola mundo')
+
+mi_funcion()    # Llamamos a la función.
