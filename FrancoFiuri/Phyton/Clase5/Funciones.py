9367f3284eb5e9d659be3085a9a98482bd8639c0
Maria Elena Haydee Ponce <programacion.ponceme@gmail.com>
2023-10-07 14:48:24 -0300

    Organizando carpetas PonceElena

--- FrancoFiuri/Phyton/Clase5/Funciones.py
+++
@@ -1,8 +0,0 @@
-# Funciones
-# mi funcion() no se puede llamar antes de definir a una función
-# Definimos una función
-def mi_funcion():   # Se debe respetar la tabulacion para que este dentro de la función.
-    print('Hola mundo')
-
-mi_funcion()    # Llamamos a la función.
-mi_funcion()  # Se puede llamar N cantidad de veces
