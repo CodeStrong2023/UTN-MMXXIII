3ee9f06f84767d31782a0657f242afea960aceb8
Gonzalo <34680110ax@gmail.com>
2023-10-11 10:25:45 -0300

    clase 7 de js

--- Gonzalo Martinez/javaScript/leccion2/02-01-Concatenaciones.js
+++ Gonzalo Martinez/javaScript/leccion2/02-01-Concatenaciones.js
@@ -8,3 +8,26 @@ var juntos =  nombre + 78 + 17;
 console.log(juntos);
 juntos = 78 + 17 + nombre;
 console.log(juntos);
+
+//tercera concatenacion
+nombre += apellido;
+console.log(nombre);
+
+// ya no se usa var se usa let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Perez";
+//apellido2 = "Peres";  una coonstante no puede ser modificada
+console.log(apellido2);
+
+let s, y; // se pueden crear varias  variables en una sola linea
+x=17, y=21;//se pueden asignar vARIAS VARIABLES EN LA MISMA LINEA
+let z = x + y;
+console.log(z);
+
+let _1num = 31;// No utilizar numeros apra inicar variables
+let rompiendo = "rompe";//No utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
---
+++ Gonzalo Martinez/javaScript/leccion3/03-01-OperadoresJavaScript.js
@@ -0,0 +1,15 @@
+// Ejercicio para encontrar numeros pares
+let parInpar = 10;
+if (parInpar % 2 == 0) {
+    console.log('El numero es par');
+} else {
+    console.log('El numero es impar');
+}
+
+//Ejercicio: es mayor de edad
+let edad = 20, adulto = 18;
+if (edad >= adulto) {
+    console.log('Usted es una persona adulta');
+} else {
+    console.log('Usted es una persona menor de edad');
+}
