4c464966ce5fb0fe06afd563a89b5b997d5e7ec6
Franco Fiuri <franco.fiuri@gmail.com>
2023-10-12 13:00:48 -0300

    Agregando Clase 5 - Java

--- FrancoFiuri/Java/Clase 4/Aritmetica/src/Operaciones/Aritmetica.java
+++ FrancoFiuri/Java/Clase 4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -13,4 +13,14 @@ public class Aritmetica {   //las clases se escriben en Pascalcase
         System.out.println("resultado = " + resultado);
     }
     
+    public int sumarConRetorno(){
+        int resultado = a + b;
+        return resultado;
+    }
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; //El argumento a se asigna al atributo this.a
+        this.b = b; //El argumento this se utiliza para separar argumento de variable.
+        //return a + b;
+        return sumarConRetorno();
+    }
 }
