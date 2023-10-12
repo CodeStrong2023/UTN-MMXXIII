ad6d2c4a149147ba11461390beb2fffc5ee1fade
Franco Fiuri <franco.fiuri@gmail.com>
2023-10-12 10:58:01 -0300

    Agregando Clase 4 - Java

---
+++ FrancoFiuri/Java/Clase 4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,16 @@
+
+package Operaciones;
+
+public class Aritmetica {   //las clases se escriben en Pascalcase
+    //Atributos de la clase
+    int a;
+    int b;
+    
+    
+    //Metodo
+    public void sumarNumeros(){ //los metodos se escriben en camelCase
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    
+}
