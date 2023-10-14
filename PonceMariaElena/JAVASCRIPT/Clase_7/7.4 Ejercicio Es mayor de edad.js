4d5776185991a0c25c4de6c8fdd7e33bb5a85ba2
Maria Elena Haydee Ponce <programacion.ponceme@gmail.com>
2023-10-14 19:58:26 -0300

    Clase 7 js

---
+++ PonceMariaElena/JAVASCRIPT/Clase_7/7.4 Ejercicio Es mayor de edad.js
@@ -0,0 +1,11 @@
+//Ejercicio
+// Es mayor de edad
+
+let edad = 18,
+	adulto = 18;
+
+if (edad >= 18) {
+	console.log("Es una persona adulta");
+} else {
+	console.log("Es una persona menor de edad");
+}
