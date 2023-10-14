31a1c213c489f1b7c0800a8f696ef9680fd5de2a
Maria Elena Haydee Ponce <programacion.ponceme@gmail.com>
2023-10-14 15:26:33 -0300

    Clase 1 - JavaScript

---
+++ PonceMariaElena/JAVASCRIPT/Clase_1/Instalaciones.js
@@ -0,0 +1,19 @@
+/* CLASE 1 - JAVASCRIPT
+
+1.1 Instalaciones necesarias
+JS es un lenguaje interpretado que se puede correr en los navegadores web.
+
+Como backend corre con Node.Js
+Hay que instalarlo desde su pagina
+
+IDE VSCODE
+1.2 Extensiones
+Quokka.js - para agilizar el desarrollo de apps con JS.
+Bracket Pair Colorizer 2 - para detectar mejor los brackets.
+ESLint - Mejores Practicas.
+Prettier - Formato de codigo.
+Better Comments- agrega colores a comentarios-
+Debugger for chrome.
+Intellicode - Completado  de codigo.
+Live Server - Servidor local.
+ */
