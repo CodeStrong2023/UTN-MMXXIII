45bf7e585c8f0bb52ee6b2f9411aa426cf5c5b20
Maria Elena Haydee Ponce <programacion.ponceme@gmail.com>
2023-10-30 12:26:12 -0300

    Proyecto Integrador . Organizando referencias en Main

--- Proyecto integrador/Appetito/src/main/java/org/example/Main.java
+++ Proyecto integrador/Appetito/src/main/java/org/example/Main.java
@@ -7,7 +7,5 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Appetito appetito = new Appetito();
         appetito.Appetito();
-       // FiltroMenuLocal menu = new FiltroMenuLocal();
-       // menu.mostrarLocal();
     }
 }
