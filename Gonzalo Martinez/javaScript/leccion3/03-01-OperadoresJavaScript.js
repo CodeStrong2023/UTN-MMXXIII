45d3448358eef9d6530e9b3f3c72e03360304ca6
Gonzalo <34680110ax@gmail.com>
2023-11-02 07:11:51 -0300

    agregando clase 9 y 10 de js

--- Gonzalo Martinez/javaScript/leccion3/03-01-OperadoresJavaScript.js
+++ Gonzalo Martinez/javaScript/leccion3/03-01-OperadoresJavaScript.js
@@ -22,3 +22,32 @@ if (dentroRango >= valMin && dentroRango <= valMax) {
 } else {
     console.log('El numero esta fuera del rango establecido');
 }
+
+// Ejercicio: si el padre puede asistir al juego de su hijo
+let vacaciones = false, diaDescanso=false;
+if(vaciones || diaDescanso){
+    console.log("El padre puede aistir al juegod e su hijo")
+}else{
+    console.log("El padre NO puede aistir al juegod e su hijo")
+}
+//operador ternario
+let resultado2= 3>2?"verdadero": "falso";
+console.log(resultado2)
+let numnero =7;
+resultado2 = numero%2 ==0 ?"es numero par" : "es un numero impar";
+console.log(resultado2)
+
+//converit  String a numero
+let minumero  = "10"
+console.log(typeof minumero)
+let edad2 = Number(minumero)
+console.log(typeof edad2)
+
+if(edad2>=18){
+    console.log("puede votar")
+}else{
+    console.log("Muy joven para votar")
+}
+
+let resultado3 = edad2 >= 18?"Puede votar" : "Muy joven para votar"
+console.log(resultado3)
